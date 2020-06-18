f2e85894b3eef210037f4a59524b2172c288c9b9
nosremetnarg <grant.alex.emerson@gmail.com>
2020-06-18 08:28:41 -0500

    button fix, now printing character numbers

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,72 @@
 // Assignment code here
+const characterAmountRange = document.getElementById
+('characterAmountRange')
+const characterAmountNumver = document.getElementById
+('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+  ).concat(
+    arrayFromLowToHigh(91, 96)
+  ).concat(
+    arrayFromLowToHigh(123, 126)
+  )
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, 
+    includeNumbers, includeSymbols)
+})
 
-  passwordText.value = password;
-
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  String.fromCharCode(65)
+  console.log(LOWERCASE_CHAR_CODES)
 }
 
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", function(){
-  alert("Lets Create a password");
-});
+function arrayFromLowToHigh(low, high){
+  const array = []
+  for (let i = low; i < high; i++){
+    array.push(i)
+  }
+  return array
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
+
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// // generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", function(){
+//   alert("Lets Create a password");
+// });
