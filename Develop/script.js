b14c2f276ca791b7627008b87b288ff7308f8f65
nosremetnarg <grant.alex.emerson@gmail.com>
2020-06-20 17:09:02 -0500

    final alert and css styling

--- Develop/script.js
+++ Develop/script.js
@@ -17,27 +17,27 @@ var confirmLowerCase;
 
 //
 function generatePassword() {
-  var confirmLength = window.prompt("How many characters would you like your password to contain?");
+  var confirmLength = window.prompt("How many characters would you like your password to contain? Please pick a number between 8 & 128.");
 
   while(confirmLength <= 7 || confirmLength >= 129) {
     alert("Password length must be between 8-128 characters Try again");
     var confirmLength = window.prompt("How many characters would you like your password to contain?")
   }
 
-  alert("Your password will be " + confirmLength + " characters");
+  alert("Your password will be " + confirmLength + " characters.");
 
   // Get parameters from user
-   var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-   var confirmNumberCharacter = confirm("Click OK to confirm if you would like to include number characters");
-   var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-   var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+   var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters.");
+   var confirmNumberCharacter = confirm("Click OK to confirm if you would like to include number characters.");
+   var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters.");
+   var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters.");
     // Loop if answer is outside parameters
     while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumberCharacter === false) {
       alert("You must pick one option");
-      var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-      var confirmNumberCharacter = confirm("Click OK to confirm if you would like to include number characters");
-      var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-      var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+      var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters.");
+      var confirmNumberCharacter = confirm("Click OK to confirm if you would like to include number characters.");
+      var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters.");
+      var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters.");
       
     }
 
@@ -76,9 +76,16 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
+  alert("Your new super cool password is " + password);
   passwordText.value = password;
-}
+} 
+
+
+// a second way to write a random pw gen is below
+
+
+
 // var promptPassword = window.prompt("Would you like to generate a password?");
 // promptPassword = promptPassword.toLowerCase();
 // if (promptPassword === "yes" || promptPassword === "YES") {
